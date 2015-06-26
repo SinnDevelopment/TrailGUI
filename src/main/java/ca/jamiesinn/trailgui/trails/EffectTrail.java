987de484022417bf03ea680effab67bdf9f48ae0
kukelekuuk00 <kukelekuuk00@radthorne.info>
2015-06-26 14:13:08 +0200

    missing classes.
    Because intellij doesn't do it automatically. Bad Intellij.

---
+++ src/main/java/ca/jamiesinn/trailgui/trails/EffectTrail.java
@@ -0,0 +1,28 @@
+package ca.jamiesinn.trailgui.trails;
+
+import org.bukkit.Effect;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+
+public class EffectTrail extends Trail
+{
+    Effect effect;
+
+    public EffectTrail(ConfigurationSection config)
+    {
+        super(config);
+        loadType(config.getString("type"));
+    }
+
+    @Override
+    protected void loadType(String sType)
+    {
+        this.effect = Effect.valueOf(sType);
+    }
+
+    @Override
+    public void justDisplay(Player player)
+    {
+        player.getWorld().playEffect(player.getLocation(), this.effect, 1);
+    }
+}
---
+++ src/main/java/ca/jamiesinn/trailgui/trails/ItemTrail.java
@@ -0,0 +1,35 @@
+package ca.jamiesinn.trailgui.trails;
+
+import com.darkblade12.particleeffect.ParticleEffect;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+
+public class ItemTrail extends Trail
+{
+    ParticleEffect.ItemData data;
+    int itemData;
+
+    public ItemTrail(ConfigurationSection config)
+    {
+        super(config);
+        itemData = config.getInt("data", 0);
+        data = new ParticleEffect.ItemData(itemType, (byte) itemData);
+        loadType(config.getString("type"));
+    }
+
+    @Override
+    protected void loadType(String sType)
+    {
+        this.type = ParticleEffect.valueOf(sType);
+    }
+
+    @Override
+    public void justDisplay(Player player)
+    {
+        if(type == null)
+        {
+            return;
+        }
+        type.display(data, player.getLocation().getDirection(), speed, player.getLocation().add(0.0D, displayLocation, 0.0D), range);
+    }
+}
